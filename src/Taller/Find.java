e27a951f748381a7cd5a09577ef20bbdbc410e7d
Gonzalo <gonzalo.rubio.gutierrez@gmail.com>
2024-01-17 09:37:42 +0100

    1 commit

---
+++ src/Taller/Find.java
@@ -0,0 +1,35 @@
+package Taller;
+import static com.mongodb.client.model.Filters.*;
+
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Projections;
+import com.mongodb.client.model.Sorts;
+
+public class Find {
+	
+	public static void main(String[] args) {
+		MongoClient mongoClient = MongoDB.getClient();
+		MongoDatabase database = mongoClient.getDatabase("Taller");
+		MongoCollection<Document> collection = database.getCollection("movies");
+		Bson projectionFields = Projections.fields(
+				Projections.include("title", "imdb"),
+				Projections.excludeId());
+		MongoCursor<Document> cursor = collection.find(lt("runtime", 15))
+				.projection(projectionFields)
+				.sort(Sorts.descending("title")).iterator();
+		try {
+		     while(cursor.hasNext()) {
+		         System.out.println(cursor.next().toJson());
+		     }
+		} finally {
+		     cursor.close();
+		}
+	}
+
+}
---
+++ src/Taller/MongoDBHandler.java
@@ -0,0 +1,35 @@
+package Taller;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+public class MongoDBHandler {
+
+    private final MongoClient mongoClient;
+    private final MongoDatabase database;
+
+    public MongoDBHandler(String connectionString, String databaseName) {
+        this.mongoClient = MongoClients.create(connectionString);
+        this.database = mongoClient.getDatabase(databaseName);
+    }
+
+    public void createCollection(String collectionName) {
+        database.createCollection(collectionName);
+        System.out.println("Colección '" + collectionName + "' creada exitosamente.");
+    }
+
+    public void closeConnection() {
+        mongoClient.close();
+    }
+
+    public static void main(String[] args) {
+        // Ejemplo de uso
+        MongoDBHandler mongoDBHandler = new MongoDBHandler("mongodb+srv://gonzalorubio:Moral0315@cluster0.w4wdn0h.mongodb.net/", "Taller");
+        mongoDBHandler.createCollection("miColeccion");
+        mongoDBHandler.closeConnection();
+    }
+}
+
